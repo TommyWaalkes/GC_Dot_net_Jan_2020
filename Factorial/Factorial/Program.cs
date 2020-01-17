7c5832bef6d509aa4266e67ac09c5f37060e9a64
Tommy Waalkes <Tommy@grandcircus.co>
2020-01-16 21:57:46 -0500

    Added early code snippets

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SetupFactorial(20));
+        }
+
+        public static int SetupFactorial(int num)
+        {
+           return RecurFactorial(num, 1);
+        }
+
+        public static int RecurFactorial(int num, int total)
+        {
+            total *= num;
+            num--;
+            if (num <= 1)
+            {
+                return total;
+            }
+            else
+            {
+                return RecurFactorial(num, total);
+            }
+        }
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool goOn = true;
+
+            while (goOn == true)
+            {
+                Console.WriteLine("Welcome to our Calculator");
+
+                Console.WriteLine("Please Enter a Length");
+                string l = Console.ReadLine();
+                double length = double.Parse(l);
+
+                Console.WriteLine("Please Enter a Width");
+                string w = Console.ReadLine();
+                double width = double.Parse(w);
+
+                double perimeter = GetPerimeter(length, width);
+                double area = GetArea(length, width);
+
+                Console.WriteLine("The perimeter is: " + perimeter);
+                Console.WriteLine("The Area is: " + area);
+
+                string response = "";
+                goOn = ContinueLoop();
+                //while (response != "y" || response != "n")
+                //{
+                //    Console.WriteLine("Would you like to continue? Y/N");
+                //    response = Console.ReadLine();
+
+                //    if (response.ToLower() == "y")
+                //    {
+                //        break;
+                //    }
+                //    else if (response.ToLower() == "n")
+                //    {
+                //        goOn = false;
+                //        break;
+                //    }
+                //}
+
+            }
+        }
+
+        public static double GetPerimeter(double length, double width)
+        {
+            double perimeter = (2 * length) + (2 * width);
+            return perimeter;
+        }
+
+        public static double GetArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        public static bool ContinueLoop()
+        {
+            Console.WriteLine("Would you like to input another room? y/n");
+            string response = Console.ReadLine().ToLower().Trim();
+
+            if(response == "y")
+            {
+                return true;
+            }
+            else if(response == "n")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Hey I didn't understand that let's try again.");
+                return ContinueLoop();
+            }
+        }
+    }
+}
