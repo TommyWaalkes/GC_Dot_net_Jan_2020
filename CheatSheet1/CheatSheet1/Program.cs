20ddca5960647840e7ee6be21b58a2552e7564de
Tommy Waalkes <Tommy@grandcircus.co>
2020-01-27 18:13:52 -0500

    Added Enum and Cheat sheet

---
+++ CheatSheet1/CheatSheet1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CheatSheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        //Loops use: 
+
+        //If Statements 
+
+        //Methods 
+
+
+    }
+}
---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    public enum WeekDays
+    {
+        Monday, 
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    public enum OrderSize
+    {
+        Small, 
+        Medium, 
+        Large, 
+        SuperSize
+    }
+}
